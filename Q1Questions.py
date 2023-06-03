3e232b76e0a6daae78373ceb542bbbf59a0e79c4
moltovow <94055753+miggylol@users.noreply.github.com>
2023-06-03 06:38:11 +0000

    surfs up 2 am

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,22 @@
-# Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
-# This will be six lines, and then store the questions in the file questions.txt.
+def main():
+    with open('questions.txt', 'w') as file:
+        for _ in range(3):  # Assuming you want to store 3 questions
+            question = input("Enter the question: ")
+            answer1 = input("Enter the first answer: ")
+            answer2 = input("Enter the second answer: ")
+            answer3 = input("Enter the third answer: ")
+            answer4 = input("Enter the fourth answer: ")
+            correct_answer = input("Enter the letter of the correct answer: ")
+
+            file.write(f"{question}\n")
+            file.write(f"{answer1}\n")
+            file.write(f"{answer2}\n")
+            file.write(f"{answer3}\n")
+            file.write(f"{answer4}\n")
+            file.write(f"{correct_answer}\n")
+
+    print("Questions stored in questions.txt.")
+
+
+if __name__ == '__main__':
+    main()
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,28 @@
-# Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
-# The program will keep score of the number of questions answered correctly.
+def main():
+    score = 0
+
+    with open('questions.txt', 'r') as file:
+        lines = file.readlines()
+        num_questions = len(lines) // 6
+
+        for i in range(num_questions):
+            question = lines[i * 6].strip()
+            answers = lines[i * 6 + 1:i * 6 + 5]
+            correct_answer = lines[i * 6 + 5].strip()
+
+            print(f"\nQuestion {i+1}: {question}")
+            for j, answer in enumerate(answers):
+                print(f"{chr(ord('A') + j)}. {answer.strip()}")
+
+            user_answer = input("Enter your answer (A, B, C, or D): ")
+            if user_answer.upper() == correct_answer:
+                print("Correct!")
+                score += 1
+            else:
+                print(f"Incorrect. The correct answer is {correct_answer}.")
+
+    print(f"\nQuiz completed. Your score is {score}/{num_questions}.")
+
+
+if __name__ == '__main__':
+    main()
